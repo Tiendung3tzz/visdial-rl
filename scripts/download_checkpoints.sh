085a8fcbcab3a3fccdc697c01aef9b557747c8a7
Dung3t <tiendung3t@gmail.com>
2025-04-28 00:20:31 +0700

    first commit

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -1,5 +1,6 @@
 
 mkdir -p /kaggle/working/visdial-rl/checkpoints
+cd /kaggle/working/visdial-rl/checkpoints
 
 gdown --id 1ONO67T_fcSKF8PKSUDiuyqSzN0ux4WRX -O abot_sl_ep60.vd
 gdown --id 1JDEqHKVSL4-CZ4r9RgXjBRcfruQ1Jk3C -O qbot_sl_ep60.vd
