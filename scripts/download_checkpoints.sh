d8f8f3441a3e528d38566e091572baa979660f8d
Dung3t <tiendung3t@gmail.com>
2025-06-29 12:06:12 +0700

    update model

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -3,17 +3,19 @@ mkdir -p /kaggle/working/visdial-rl/checkpoints
 cd /kaggle/working/visdial-rl/checkpoints
 
 gdown --id 1ONO67T_fcSKF8PKSUDiuyqSzN0ux4WRX -O abot_sl_ep60.vd
-
 gdown --id 1JDEqHKVSL4-CZ4r9RgXjBRcfruQ1Jk3C -O qbot_sl_ep60.vd
 
 gdown --id 15jcIynNT4S8OqdS6ZibuC7kXK3CMA4YJ -O abot_rl_ep20.vd
 gdown --id 16B6Cf93W7N8QDbnDbFkAvuvcAFlgxZQu -O qbot_rl_ep20.vd
 
-gdown --id 19USVuFlINkoeiwlY0FhN5MXB45faPTRn -O abot_ep_6.vd
-gdown --id 1Nksj0nv2pRaNGcu7hmRnn9uw_GGpuCSl -O qbot_ep_6.vd
+# gdown --id 19USVuFlINkoeiwlY0FhN5MXB45faPTRn -O abot_ep_6.vd
+# gdown --id 1Nksj0nv2pRaNGcu7hmRnn9uw_GGpuCSl -O qbot_ep_6.vd
 
-gdown --id 1VNdsqCmop-0tGIA_tc9G8jELWv_r8vzy -O abot_rl_ep_2.vd
-gdown --id 18Jgc78ojMF7JI2fNFjpSARnim4b1KZrr -O qbot_rl_ep_2.vd
+# gdown --id 1VNdsqCmop-0tGIA_tc9G8jELWv_r8vzy -O abot_rl_ep_2.vd
+# gdown --id 18Jgc78ojMF7JI2fNFjpSARnim4b1KZrr -O qbot_rl_ep_2.vd
 
 gdown --id 12Kz-OjIJD9UI-gcbgQcXuV5HnQ3gmY46 -O abot_sl_ep20.vd
 gdown --id 1cVZmzRoKiGH2u_meIGxBc_ZA4707Ivkh -O qbot_sl_ep20.vd
+
+gdown --id 1PmJ38F0iSnKNxF1sGMsAmDD6gYjRZatO -O abot_rl_ep6.vd
+gdown --id 1kLCtHO_1R-X95CRguXW9ybyXSiTj3kvc -O qbot_rl_ep6.vd
